e3824d9e7e7bb760f87fe587ab07f7e3425e6be4
Nick Barrett <pointlessrambler@gmail.com>
2017-10-02 09:57:55 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+if __name__ == '__main__':
+    setup(
+        version='0.1',
+        name='pyinfra-etcd',
+        description='Install & bootstrap etcd clusters with pyinfra.',
+        packages=find_packages(),
+        install_requires=('pyinfra>=0.5'),
+    )
