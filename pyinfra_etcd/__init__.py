f30445fec39d2b11fdb2b453dec59fd52654ede1
Nick Barrett <pointlessrambler@gmail.com>
2017-10-11 14:09:20 +0100

    Make the `deploy_etcd` wrapper function a proper `@deploy` so it supports pyinfra's global kwargs.

--- pyinfra_etcd/__init__.py
+++ pyinfra_etcd/__init__.py
@@ -2,9 +2,12 @@
 # File: pyinfra_etcd/__init__.py
 # Desc: export deploys and install/configure helper function
 
+from pyinfra.api import deploy
+
 from .etcd import configure_etcd, install_etcd
 
 
-def deploy_etcd(enable_service=True):
-    install_etcd()
-    configure_etcd(enable_service=enable_service)
+@deploy('Deploy etcd')
+def deploy_etcd(state, host, enable_service=True):
+    install_etcd(state, host)
+    configure_etcd(state, host, enable_service=enable_service)
