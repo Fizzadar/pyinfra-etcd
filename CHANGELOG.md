ac6395551dac202d5df71528b114fdb8f79548b3
Nick Barrett <pointlessrambler@gmail.com>
2017-11-28 11:00:36 +0000

    v0.1.1 fixing package data.

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# v0.1.1
+
++ Fix/setup `MANIFEST.in` and package data
+
+# v0.1
+
++ Initial release.
--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import find_packages, setup
 
 if __name__ == '__main__':
     setup(
-        version='0.1',
+        version='0.1.1',
         name='pyinfra-etcd',
         description='Install & bootstrap etcd clusters with pyinfra.',
         packages=find_packages(),
         install_requires=('pyinfra>=0.5'),
+        include_package_data=True,
     )
